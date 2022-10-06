7a6ed9f3f4d5fea6e44606b29a7822173ab82b73
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2022-10-06 00:38:30 -0400

    Update PremiumMember.java

--- PremiumMember.java
+++ PremiumMember.java
@@ -3,22 +3,28 @@ public class PremiumMember {
     private String name;
     private String address;
     private String email;
-    public int premiumMemberID = 1;
+    private int premiumMemberID = 1;
+    private double totalSpending;
+    private String cardInfo;
+    private boolean monthlyFeePaid = false;
 
     public PremiumMember(){}
 
-    public PremiumMember(String name, String address, String email, int premiumMemberID) {
+    public PremiumMember(String name, String address, String email, int premiumMemberID, String cardInfo, boolean monthlyFeePaid, double totalSpending) {
         this.name = name;
         this.address = address;
         this.email = email;
         this.premiumMemberID = premiumMemberID;
+        this.cardInfo = cardInfo;
+        this.totalSpending = totalSpending;
+        this.monthlyFeePaid = monthlyFeePaid;
     }
 
     public String getName(){
         return name;
     }
 
-    public String getaddress() {
+    public String getAddress(){
         return address;
     }
 
@@ -26,7 +32,27 @@ public class PremiumMember {
         return email;
     }
 
-    public int getMemberID() {
+    public int getPremiumMemberID(){
         return premiumMemberID;
+    }
+
+    public String getCardInfo(){
+        return cardInfo;
+    }
+
+    public void setCardInfo(String cardInfo){
+        this.cardInfo = cardInfo;
+    }
+
+    public double getTotalSpending(){
+        return totalSpending;
+    }
+
+    public void setTotalSpending(double total){
+        totalSpending += total;
+    }
+
+    public boolean isMonthlyFeePaid(){
+        return monthlyFeePaid;
     }
 }
