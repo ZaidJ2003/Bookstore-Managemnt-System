200bf81b240c70d04eba984af2644a999c6896d8
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2022-10-06 00:37:43 -0400

    Update Member.java

--- Member.java
+++ Member.java
@@ -4,21 +4,23 @@ public class Member {
     private String address;
     private String email;
     private int memberID = 1;
+    private double totalSpending = 0.0;
 
     public Member(){}
 
-    public Member(String memberName, String address, String email, int memberID){
+    public Member(String memberName, String address, String email, int memberID, double totalSpending){
         this.memberName = memberName;
         this.address = address;
         this.email = email;
         this.memberID = memberID;
+        this.totalSpending = totalSpending;
     }
 
     public String getMemberName() {
         return memberName;
     }
     
-    public String getaddress() {
+    public String getAddress() {
         return address;
     }
 
@@ -28,5 +30,13 @@ public class Member {
 
     public int getMemberID() {
         return memberID;
+    }
+
+    public double getTotalSpending(){
+        return totalSpending;
+    }
+
+    public void setTotalSpending(double total){
+        totalSpending += total;
     }
 }
