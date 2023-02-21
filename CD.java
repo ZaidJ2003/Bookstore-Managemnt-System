66a5369f4441bcbdf858da9690ffa9423e7d7a2e
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2023-02-21 17:26:24 -0500

    Update CD.java

--- CD.java
+++ CD.java
@@ -1,6 +1,6 @@
 public class CD extends Product{
 
-    public CD(String name, double price, int orderNum, int stock){
-        super(name, price, orderNum, stock);
+    public CD(String name, String author, double price, int orderNum, int stock, int bought){
+        super(name, author, price, orderNum, stock, bought);
     }
 }
