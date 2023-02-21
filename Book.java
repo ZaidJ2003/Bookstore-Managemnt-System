eb02e562f290e4d6e86aecc1e25bed337d84698f
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2023-02-21 17:22:14 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,6 @@
 public class Book extends Product{
 
-    public Book(String name, double price, int orderNum, int stock){
-        super(name, price, orderNum, stock);
+    public Book(String name, String author, double price, int orderNum, int stock, int bought){
+        super(name, author, price, orderNum, stock, bought);
     }
 }
