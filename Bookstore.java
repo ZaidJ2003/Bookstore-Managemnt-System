db1064711c809f19fa1a41b5a03a58a70338cfd2
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2023-02-21 17:23:12 -0500

    Update Bookstore.java

--- Bookstore.java
+++ Bookstore.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.lang.Math;
+import java.io.*;
+import java.util.Scanner;
 
 public class Bookstore implements BookStoreSpecification{
 
     private ArrayList<Product> productInventory = new ArrayList<>();
     private ArrayList<Member> memberInventory = new ArrayList<>();    
-    private int orderNum = 1;
     private int memberID = 1;
     private int itemsInCart;
     private double total;
@@ -14,41 +15,24 @@ public class Bookstore implements BookStoreSpecification{
     private boolean cdEmpty = false;
     private boolean dvdEmpty = false;
     private ArrayList<Product> productTracker = new ArrayList<>();
+    private ArrayList<Product> productTrackerReport = new ArrayList<>();
     private int books;
     private int cds;
     private int dvds;
+    private int members;
+    private int premMembers;
 
-    public Bookstore(){ //Adds default values in each list
+    public Bookstore() { //Adds values in each list
         System.out.println("Welcome to The Bookstore Management System!\n");
-        generateInventory();
         generateMembers();
-    }
-    
-
-    private void generateInventory(){ //Method that generates defauly products
-        productInventory.add(new Book("Harry Potter", 17.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new Book("Fifty Shades of Grey", 15.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new Book("Twilight", 10.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new Book("Life of Pi", 14.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new CD("Thriller", 2.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new CD("Back in black", 1.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new CD("The Bodygaurd", 2.49, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new DVD("TOP GUN:MAVERICK", 19.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new DVD("DOCTOR STRANGE" , 16.99, orderNum, 3));
-        incrementOrderNum();
-        productInventory.add(new DVD("SPIDER MAN", 15.99, orderNum, 3));
-        incrementOrderNum();
+        try{
+        populateInventory();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found.");
+        }
     }
 
-    private void generateMembers(){ //Method that generates defauly products
+    private void generateMembers(){ //Method that generates default members
         memberInventory.add(new Member("John", "4343 road", "John@yahoo.com", memberID, 0));
         incrementMemberID();
         memberInventory.add(new Member("smith", "5431 road", "Smith@yahoo.com", memberID,0));
@@ -61,6 +45,65 @@ public class Bookstore implements BookStoreSpecification{
         incrementMemberID();
         memberInventory.add(new PremiumMember("Logan", "5632 road", "Logan@yahoo.com", memberID,0, "14@4",true ));
         incrementMemberID();
+    }
+    private void populateInventory() throws FileNotFoundException{ //method that reads data from file and places them in arraylist
+        Scanner fileScanner = new Scanner(new File("/Users/zaid_jebril/Desktop/ProductInventory.csv"));
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            if (line.contains("book") || line.contains("cd") || line.contains("dvd")){
+                String[] lineArray = line.split(",");
+                String name = lineArray[2];
+                String author = lineArray[3];
+                double price = Double.parseDouble(lineArray[5]);
+                int orderNum = Integer.parseInt(lineArray[0]);
+                int stock = Integer.parseInt(lineArray[4]);
+                if(line.contains("book")){
+                    Product obj = new Book(name, author, price, orderNum, stock, 1);
+                    productInventory.add(obj);
+                }
+                else if(line.contains("cd")){
+                    Product obj = new CD(name, author, price, orderNum, stock, 1);
+                    productInventory.add(obj);                   
+                }
+                else{
+                    Product obj = new DVD(name, author, price, orderNum, stock, 1);
+                    productInventory.add(obj);
+                }
+            }
+        }
+    }
+
+    public void generateEndOfDayReport() throws IOException{ //Method that generates an end of day report file
+        FileOutputStream fs = new FileOutputStream("EndOfDayReport.txt");
+        PrintWriter pw = new PrintWriter(fs);
+        double total = 0.0;
+
+        pw.println("End of Day Report: ");
+        pw.println("products purchased:");
+        
+        for (Product i: productTrackerReport) {
+            pw.println(i.getBought() + " copies of " + i.getClass() + " " + i.getName());
+            total += (i.getBought() * i.getPrice());
+        }
+
+        pw.println("Number of new members registered:\nmembers: " + members + "\npremium members: " + premMembers);
+        pw.println("Total sales: $" + total);
+        
+        pw.close();
+        fs.close();
+    }
+
+    public void generateBookInventoryDay2() throws IOException{ //method that generates an inventory for the next day
+        FileOutputStream fs = new FileOutputStream("BookInventoryDay2.csv");
+        PrintWriter pw = new PrintWriter(fs);
+
+        pw.println("product ID Type title author/artist numInStock Price");
+        for(Product i : productInventory){
+            pw.println(i.getOrderNum() + "," + i.getName() + "," + i.getAuthor() + "," + i.getStock() + "," + i.getPrice());
+        }
+        
+        pw.close();
+        fs.close();
     }
 
     public void Purchase(int orderNum){ //Method that allows user to purchase a product
@@ -91,11 +134,19 @@ public class Bookstore implements BookStoreSpecification{
         }
     }
     
-    public void resetProductTracker(){ //Method that removes all elements in productTracker list
+    public void resetProductTracker(){ //Method that removes all elements in productTracker list and places them in a different list for end of day report
+        for(Product s : productTracker){
+            if(productTrackerReport.contains(s)){
+                s.incrementBought();
+            }
+            else{
+            productTrackerReport.add(s);
+            }
+        }
+
         if(!productTracker.isEmpty()){
             productTracker.removeAll(productTracker);
         }
-
     }
 
     public void addAllToProduct(){ //Method that adds all elemnts of productTracker list to productInventory list
@@ -106,7 +157,7 @@ public class Bookstore implements BookStoreSpecification{
                 }
                 else if(!productInventory.contains(productTracker.get(i))){
                     productInventory.add(productTracker.get(i));
-                    restockProduct(productTracker.get(i).getOrderNum(), productTracker.get(i).getPrice());
+                    restockProduct(productTracker.get(i).getOrderNum(), productTracker.get(i).getStock()+1);
                 }
             }
         }
@@ -157,6 +208,7 @@ public class Bookstore implements BookStoreSpecification{
         System.out.println("Success! you are now a Book Store Member, " + name + ". Your ID is " + memberID + ". Make Sure to store this number because you will need it for verification!");
         System.out.println("Total money spent: " + totalSpending);
         incrementMemberID();
+        members++;
     }
     
     public void addPremiumMember(String name, String Address, String email, String cardInfo, boolean MonthlyFeePaid ,double totalSpending){ // Method that allows user to enroll as a premium member
@@ -168,6 +220,7 @@ public class Bookstore implements BookStoreSpecification{
         }
         System.out.println("Total money spent: $" + totalSpending);
         incrementMemberID();
+        premMembers++;
     }
 
     public boolean isMember(int memberID){ // Method that verifies If user is a member.
@@ -239,10 +292,6 @@ public class Bookstore implements BookStoreSpecification{
         double newTotal = total -= discount;
         return Math.round(newTotal*100.0)/100.0;
     }
-    
-    private void incrementOrderNum(){ //Method that increments OrderNum by 1
-        orderNum++;
-    }
 
     private void incrementMemberID(){ //Method that increments MemberID by 1
         memberID++;
@@ -288,10 +337,10 @@ public class Bookstore implements BookStoreSpecification{
     }
 
     @Override
-    public void restockProduct(int id, double price) { //Method that restocks products if out of stock
+    public void restockProduct(int id, int stock) { //Method that restocks products if out of stock
         for(Product i : productInventory){
-            if(i.getOrderNum() == id && i.getPrice() == price){
-                i.incrementStock();
+            if(i.getOrderNum() == id){
+                i.setStock(stock);
             }
         }
     }
